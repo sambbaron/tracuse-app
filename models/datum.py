bf750f3578e1742ed4bd476fa0db7af63f889adb
Sam Baron <sambaron@ownmail.net>
2015-04-22 08:32:30 -0700

    Add active column to DefinitionMixin

--- models/datum.py
+++ models/datum.py
@@ -2,8 +2,8 @@
 
 from datetime import datetime
 
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, Index
+from sqlalchemy import ForeignKey, Integer, String, DateTime, Boolean
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.hybrid import hybrid_property
 
@@ -16,17 +16,18 @@ class DefinitionMixin(object):
     """Common properties for models that define entity concept
 
     Attributes:
-        sort: integer
-        name: string
-        short_description: string -> Word or two about object
-        long_description: string -> Long description of object
-        schema_name: string -> Underscore/lower-case class name
+        sort (integer)
+        name (string, indexed)
+        short_description (string): Word or two about object
+        long_description (string): Long description of object
+        schema_name (string): Underscore/lower-case class name
     """
 
     sort = Column(Integer, default=0)
     name = Column(String(25), index=True)
     short_description = Column(String(25))
     long_description = Column(String(100))
+    active = Column(Boolean, default=True, index=True)
 
     def __str__(self):
         return "<{class_name}(Name={instance_name})>".format(
