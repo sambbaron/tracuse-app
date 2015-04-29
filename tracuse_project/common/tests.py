6adb9f48e70dabde506f3f0e4946c2cce7f978de
Sam Baron <sambaron@ownmail.net>
2015-04-29 11:46:25 -0700

    Add common.utils tests for string manipulation functions
    Using unittest.TestCase, still deciding on whether to use
    built-in Django testing tools.

--- tracuse_project/common/tests.py
+++ tracuse_project/common/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from common import utils
+
+
+class TestStringUtils(unittest.TestCase):
+    """Test string manipulation functions
+    """
+
+    @property
+    def test_strings(self):
+        return ["First Name", "FirstName", "firstName", "First", "firstname", "FIRSTNAME"]
+
+    def test_camel_to_spaced_lower(self):
+        expected_strings = ["first name", "first name", "first name", "first", "firstname", "f i r s t n a m e"]
+        for test in self.test_strings:
+            actual = utils.camel_to_spaced_lower(test)
+            expected = expected_strings[self.test_strings.index(test)]
+            return self.assertEqual(expected, actual)
+        
+    def test_camel_to_underscore(self):
+        expected_strings = ["first_name", "first_name", "first_name", "first", "firstname", "f_i_r_s_t_n_a_m_e"]
+        for test in self.test_strings:
+            actual = utils.camel_to_underscore(test)
+            expected = expected_strings[self.test_strings.index(test)]
+            return self.assertEqual(expected, actual)
+        
+    def test_camel_to_spaced_capital(self):
+        expected_strings = ["First Name", "First Name", "First Name", "First", "Firstname", "FIRSTNAME"]
+        for test in self.test_strings:
+            actual = utils.camel_to_spaced_capital(test)
+            expected = expected_strings[self.test_strings.index(test)]
+            return self.assertEqual(expected, actual)
