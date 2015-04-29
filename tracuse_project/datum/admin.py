cca16fc81ea7c7d4387e8f9197e180be2f87f788
Sam Baron <sambaron@ownmail.net>
2015-04-28 21:29:33 -0700

    Admin testing - not working for this project
    Cannot link DatumObject with ElementValues without heavy
    customization.

--- tracuse_project/datum/admin.py
+++ tracuse_project/datum/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DatumObject
+from element_type.models import ElementTypeDatumObject
+from element_value.models import ElementValueModel, ElementValueString
+
+
+class ElementTypeInline(admin.StackedInline):
+    model = ElementTypeDatumObject
+    extra = 0
+    list_select_related = True
+    show_change_link = True
+    fields = ("element_type", "element_value" )
+    readonly_fields = ("element_type", "element_value")
+    can_delete = False
+
+
+@admin.register(DatumObject)
+class DatumAdmin(admin.ModelAdmin):
+
+    actions_on_top = True
+    save_on_top = True
+    list_select_related = True
+
+    list_display = ("__str__", "user", "active", "sort", "datum_type", "datum_group", "assigned_element_types")
+    list_editable = ("user", "active", "sort", "datum_type")
+    list_display_links = ("assigned_element_types",)
+
+    fields = (("user", "active", "sort", "datum_type"))
+    # readonly_fields = ("assigned_element_types",)
+
+    # inlines = [ElementTypeInline, ]
