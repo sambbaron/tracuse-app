61c19e94aed769e4cdc7700fe62d4cfb080ab8d7
Sam Baron <sambaron@ownmail.net>
2015-04-30 14:18:50 -0700

    Add remainder EntityMixin tests
    100% covered

--- tracuse_project/components/common/tests.py
+++ tracuse_project/components/common/tests.py
@@ -33,18 +33,67 @@ class TestStringUtils(TestCase):
             return self.assertEqual(expected, actual)
 
 
-class TestEntityMixin(TestCase):
+class TestModelEntityMixin(TestCase):
     """Test Django ORM mixin classes used by all models"""
 
-    def test_readable_name(self):
+    def setUp(self):
         from .mixins import EntityMixin
 
-        test_mixin = EntityMixin()
-        test_mixin.entity_name = "ThisObject"
+        self.test_mixin = EntityMixin()
+        self.test_mixin.entity_name = "ThisObject"
 
+    def tearDown(self):
+        self.test_mixin = None
+
+
+    def test_entity_str(self):
         with mock.patch("django.db.models.base.Model.save"):
-            test_mixin.save()
+            self.test_mixin.save()
 
-        actual = test_mixin.readable_name
+        actual = self.test_mixin.__str__()
         expected = "This Object"
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
+
+    def test_readable_name_input(self):
+        self.test_mixin.readable_name = "Custom Readable Name"
+        actual = self.test_mixin.readable_name
+        expected = "Custom Readable Name"
+        self.assertEqual(expected, actual)
+
+    def test_schema_name_default(self):
+        with mock.patch("django.db.models.base.Model.save"):
+            self.test_mixin.save()
+
+        actual = self.test_mixin.schema_name
+        expected = "this_object"
+        self.assertEqual(expected, actual)
+
+    def test_schema_name_input(self):
+        self.test_mixin.schema_name = "custom_schema_name"
+        actual = self.test_mixin.schema_name
+        expected = "custom_schema_name"
+        self.assertEqual(expected, actual)
+
+    def test_plural_name_default_s(self):
+        with mock.patch("django.db.models.base.Model.save"):
+            self.test_mixin.save()
+
+        actual = self.test_mixin.plural_name
+        expected = "This Objects"
+        self.assertEqual(expected, actual)
+
+    def test_plural_name_default_es(self):
+        self.test_mixin.entity_name = "ThisStatus"
+        with mock.patch("django.db.models.base.Model.save"):
+            self.test_mixin.save()
+
+        actual = self.test_mixin.plural_name
+        expected = "This Statuses"
+        self.assertEqual(expected, actual)
+
+    def test_plural_name_input(self):
+        self.test_mixin.plural_name = "custom plural names"
+        actual = self.test_mixin.plural_name
+        expected = "custom plural names"
+        self.assertEqual(expected, actual)
+
