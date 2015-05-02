a445db83a1831f93aeea2b801312b34d4ea99b93
Sam Baron <sambaron@ownmail.net>
2015-05-01 20:56:13 -0700

    Messing around more with Django admin, not useful
    Need to add and test model methods to create related
    records across datum and element models.

--- tracuse_project/components/datum/admin.py
+++ tracuse_project/components/datum/admin.py
@@ -1,31 +1,38 @@
 from django.contrib import admin
 
 from components.datum.models import DatumObject
-from components.element_type.models import ElementTypeDatumObject
+from components.element_type.models import ElementTypeDatumObject, ElementType
+from components.element_value.models import ElementValueModel
 
-
-class ElementTypeInline(admin.StackedInline):
+class ElementTypeInline(admin.TabularInline):
     model = ElementTypeDatumObject
-    extra = 0
+
+    fields = (("active", "sort", "element_type", "element_value"), )
+    readonly_fields = ("element_value",)
+    extra = 1
     list_select_related = True
     show_change_link = True
-    fields = ("element_type", "element_value" )
-    readonly_fields = ("element_type", "element_value")
-    can_delete = False
 
 
 @admin.register(DatumObject)
 class DatumAdmin(admin.ModelAdmin):
-
     actions_on_top = True
     save_on_top = True
     list_select_related = True
 
-    list_display = ("__str__", "user", "active", "sort", "datum_type", "datum_group", "assigned_element_types")
+    list_display = ("__str__", "user", "active", "sort", "datum_type", "datum_group")
     list_editable = ("user", "active", "sort", "datum_type")
-    list_display_links = ("assigned_element_types",)
 
-    fields = (("user", "active", "sort", "datum_type"))
-    # readonly_fields = ("assigned_element_types",)
+    fields = (("user", "active", "sort", "datum_type"),)
+    readonly_fields = ("assigned_element_types",)
 
-    # inlines = [ElementTypeInline, ]
+    inlines = [ElementTypeInline, ]
+
+
+class ElementValuesInline(admin.TabularInline):
+    model = ElementValueModel("String")
+
+@admin.register(ElementTypeDatumObject)
+class ElementTypeDatumObjectAdmin(admin.ModelAdmin):
+    pass
+    # inlines = [ElementValuesInline, ]
