4c12278483e5bc9b29bca75b021b31df55e94dbd
Sam Baron <sambaron@ownmail.net>
2015-05-02 11:10:00 -0700

    Revise DatumObject model test data
    Includes mocked objects through entire datum-element schema.
    May refactor out of this file for reuse in other component
    tests.

--- tracuse_project/components/datum/tests.py
+++ tracuse_project/components/datum/tests.py
@@ -8,22 +8,45 @@ from .models import DatumGroup, DatumType, DatumObject
 class TestModelDatumObject(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.datum_group1 = mommy.make(DatumGroup,
+        # Mock Datum objects
+        cls.datum_group1 = mommy.make("datum.DatumGroup",
                                       entity_name="TestGroup")
-        cls.datum_type1 = mommy.make(DatumType,
+        cls.datum_type1 = mommy.make("datum.DatumType",
                                      entity_name="TestType",
                                      datum_group=cls.datum_group1)
-        cls.datum_object1 = mommy.make(DatumObject,
+        cls.datum_object1 = mommy.make("datum.DatumObject",
                                        datum_type=cls.datum_type1)
 
+        # Mock Element Type objects
+        cls.element_data_type1 = mommy.make("element_type.ElementDataType",
+                                           entity_name="String"
+                                           )
+        cls.element_type1 = mommy.make("element_type.ElementType",
+                                       entity_name="TestElementType1",
+                                       element_data_type=cls.element_data_type1)
+        cls.element_type2 = mommy.make("element_type.ElementType",
+                                       entity_name="TestElementType2",
+                                       element_data_type=cls.element_data_type1)
+
+        # Mock many-to-many relationships between Datum and Element Type objects
+        cls.element_type_datum_type1 = mommy.make("element_type.ElementTypeDatumType",
+                                                make_m2m=True,
+                                                element_type = cls.element_type1,
+                                                datum_type = cls.datum_type1
+                                                )
+        cls.element_type_datum_object1 = mommy.make("element_type.ElementTypeDatumObject",
+                                                make_m2m=True,
+                                                element_type = cls.element_type1,
+                                                datum_object = cls.datum_object1
+                                                )
+
+        # Mock Element Value objects
+        cls.element_value_string1 = mommy.make("element_value.ElementValueString",
+                                               element_type_datum_object=cls.element_type_datum_object1,
+                                               element_value="Test Object Name")
+
+
     def tearDown(self):
-        pass
-
-
-    def test_str_no_name(self):
-        pass
-
-    def test_str_has_name(self):
         pass
 
     def test_datum_group(self):
