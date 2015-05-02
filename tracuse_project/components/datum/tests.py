566bd8c133f2885e7ad9c179874521431b317b5a
Sam Baron <sambaron@ownmail.net>
2015-05-01 20:54:11 -0700

    Add model_mommy and initial setup data to datum model tests

--- tracuse_project/components/datum/tests.py
+++ tracuse_project/components/datum/tests.py
@@ -1,9 +1,22 @@
 from django.test import TestCase
 
+from model_mommy import mommy
+
+from .models import DatumGroup, DatumType, DatumObject
+
 
 class TestModelDatumObject(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.datum_group1 = mommy.make(DatumGroup,
+                                      entity_name="TestGroup")
+        cls.datum_type1 = mommy.make(DatumType,
+                                     entity_name="TestType",
+                                     datum_group=cls.datum_group1)
+        cls.datum_object1 = mommy.make(DatumObject,
+                                       datum_type=cls.datum_type1)
 
-    def setUp(self):
+    def tearDown(self):
         pass
 
     def tearDown(self):
