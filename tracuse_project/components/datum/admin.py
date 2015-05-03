bc74ee97414ad11b79410ed85eec3f2eb9cbc744
Sam Baron <sambaron@ownmail.net>
2015-05-03 12:59:27 -0700

    Add models to datum.admin
    Works for DatumType on down to ElementValue, but requires
    a lot of navigation. Going to use it for now.
    Next, put element_type models in element_type.admin.

--- tracuse_project/components/datum/admin.py
+++ tracuse_project/components/datum/admin.py
@@ -1,21 +1,56 @@
 from django.contrib import admin
 
-from components.datum.models import DatumObject
+from components.datum.models import DatumType, DatumObject
 from components.element_type.models import ElementTypeDatumObject, ElementType
 from components.element_value.models import ElementValueModel
 
-class ElementTypeInline(admin.TabularInline):
+
+class DatumObjectInline(admin.TabularInline):
+    model = DatumObject
+
+    fields = (("__str__", "active", "sort"),)
+    readonly_fields = ("__str__", )
+    extra = 1
+    list_select_related = True
+    show_change_link = True
+
+
+@admin.register(DatumType)
+class DatumTypeAdmin(admin.ModelAdmin):
+    actions_on_top = True
+    save_on_top = True
+    list_select_related = True
+
+    list_display = ("__str__", "active", "sort", "entity_name",
+                    "short_definition", "long_definition",
+                    "repr_expression",
+                    "readable_name", "readable_plural_name", "schema_name")
+    list_editable = ("active", "sort", "entity_name",
+                     "short_definition", "long_definition",
+                     "repr_expression",
+                     "readable_name", "readable_plural_name", "schema_name")
+    list_display_links = ("__str__",)
+
+    fields = (("active", "sort", "entity_name"),
+              ("short_definition", "long_definition"),
+              ("repr_expression"),
+              ("readable_name", "readable_plural_name", "schema_name"),)
+
+    inlines = [DatumObjectInline, ]
+
+
+class ElementTypeDatumObjectInline(admin.TabularInline):
     model = ElementTypeDatumObject
 
-    fields = (("active", "sort", "element_type", "element_value"), )
-    readonly_fields = ("element_value",)
+    fields = ("datum_object", "element_type", "element_value",)
+    readonly_fields = ("element_value", )
     extra = 1
     list_select_related = True
     show_change_link = True
 
 
 @admin.register(DatumObject)
-class DatumAdmin(admin.ModelAdmin):
+class DatumObjectAdmin(admin.ModelAdmin):
     actions_on_top = True
     save_on_top = True
     list_select_related = True
@@ -26,17 +61,17 @@ class DatumAdmin(admin.ModelAdmin):
     fields = (("user", "active", "sort", "datum_type"),)
     readonly_fields = ("element_types",)
 
-    inlines = [ElementTypeInline, ]
+    inlines = [ElementTypeDatumObjectInline, ]
 
 
 class ElementValuesInline(admin.TabularInline):
     model = ElementValueModel("String")
 
+
 @admin.register(ElementTypeDatumObject)
 class ElementTypeDatumObjectAdmin(admin.ModelAdmin):
-
     list_display = ("datum_object", "element_type", "element_value",)
-    list_editable = ("datum_object", "element_type", )
+    # list_editable = ("datum_object", "element_type", )
     list_display_links = ("element_value",)
     list_select_related = True
 
