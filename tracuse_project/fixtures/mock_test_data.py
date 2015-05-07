d13f6a488ae9ac85af3f12399e4fc46cfeb44bb5
Sam Baron <sambaron@ownmail.net>
2015-05-06 21:01:51 -0700

    Create TestDataAssociation class with initial test data
    Datum objects with name elements and a few
    associations. May need to change as association
    methods and tests evolve.

--- tracuse_project/fixtures/mock_test_data.py
+++ tracuse_project/fixtures/mock_test_data.py
@@ -1,13 +1,13 @@
 from model_mommy import mommy
 from mock_django.managers import QuerySetMock
 
-from components.datum.models import DatumGroup
+from components.datum.models import DatumGroup, DatumObject
 
 
 class TestDataDatumElement(object):
     """Simple mock datum and element objects for testing"""
-    def __init__(self):
 
+    def __init__(self):
         # Mock User
         self.user1 = mommy.make("auth.User",
                                 username="TestUser1"
@@ -35,18 +35,18 @@ class TestDataDatumElement(object):
         self.datum_type1 = mommy.make("datum.DatumType",
                                       entity_name="TestDatumType1",
                                       datum_group=self.datum_group1,
-                                      repr_expression = "{{name}}",
+                                      repr_expression="{{name}}",
                                       sort=10100)
         # Calculate sort
         self.datum_type2 = mommy.make("datum.DatumType",
                                       entity_name="TestDatumType2",
                                       datum_group=self.datum_group1,
-                                      repr_expression = "{{name}}")
+                                      repr_expression="{{name}}")
         # Calculate sort in new group
         self.datum_type3 = mommy.make("datum.DatumType",
                                       entity_name="TestDatumType3",
                                       datum_group=self.datum_group2,
-                                      repr_expression = "{{name}}")
+                                      repr_expression="{{name}}")
 
         # Mock Datum Objects
         # Has element_type and element_value
@@ -100,4 +100,96 @@ class TestDataDatumElement(object):
         self.element_value_string1 = mommy.make("element_value.ElementValueString",
                                                 element_type_datum_object=self.element_type_datum_object1,
                                                 element_value="Test Object Name")
+
+
+class TestDataAssociation(object):
+    """Datum Objects for testing associations"""
+
+    def __init__(self):
+        # Mock Datum Objects
+        self.datum_object1 = mommy.make("datum.DatumObject")
+        self.datum_object2 = mommy.make("datum.DatumObject")
+        self.datum_object3 = mommy.make("datum.DatumObject")
+        self.datum_object4 = mommy.make("datum.DatumObject")
+        self.datum_object5 = mommy.make("datum.DatumObject")
+        self.datum_object6 = mommy.make("datum.DatumObject")
+
+        self.datum_objects_all = [self.datum_object1,
+                                 self.datum_object2,
+                                 self.datum_object3,
+                                 self.datum_object4,
+                                 self.datum_object5,
+                                 self.datum_object6,
+                                 ]
+        self.datum_group_qs = QuerySetMock(DatumObject, *self.datum_objects_all)
+
+        # Mock Element Type for Name Identification
+        self.element_data_type1 = mommy.make("element_type.ElementDataType",
+                                             entity_name="String"
+                                             )
+        self.element_type1 = mommy.make("element_type.ElementType",
+                                        entity_name="Name",
+                                        element_data_type=self.element_data_type1
+                                        )
+
+        # Assign Element Types to Datum Objects
+        self.element_type_datum_object1 = mommy.make("element_type.ElementTypeDatumObject",
+                                                     datum_object=self.datum_object1,
+                                                     element_type=self.element_type1
+                                                     )
+        self.element_type_datum_object2 = mommy.make("element_type.ElementTypeDatumObject",
+                                                     datum_object=self.datum_object2,
+                                                     element_type=self.element_type1
+                                                     )
+        self.element_type_datum_object3 = mommy.make("element_type.ElementTypeDatumObject",
+                                                     datum_object=self.datum_object3,
+                                                     element_type=self.element_type1
+                                                     )
+        self.element_type_datum_object4 = mommy.make("element_type.ElementTypeDatumObject",
+                                                     datum_object=self.datum_object4,
+                                                     element_type=self.element_type1
+                                                     )
+        self.element_type_datum_object5 = mommy.make("element_type.ElementTypeDatumObject",
+                                                     datum_object=self.datum_object5,
+                                                     element_type=self.element_type1
+                                                     )
+        self.element_type_datum_object6 = mommy.make("element_type.ElementTypeDatumObject",
+                                                     datum_object=self.datum_object6,
+                                                     element_type=self.element_type1
+                                                     )
+
+        # Mock Element Value objects
+        self.element_value_string1 = mommy.make("element_value.ElementValueString",
+                                                element_type_datum_object=self.element_type_datum_object1,
+                                                element_value="Test Object1")
+        self.element_value_string2 = mommy.make("element_value.ElementValueString",
+                                                element_type_datum_object=self.element_type_datum_object2,
+                                                element_value="Test Object2")
+        self.element_value_string3 = mommy.make("element_value.ElementValueString",
+                                                element_type_datum_object=self.element_type_datum_object3,
+                                                element_value="Test Object3")
+        self.element_value_string4 = mommy.make("element_value.ElementValueString",
+                                                element_type_datum_object=self.element_type_datum_object4,
+                                                element_value="Test Object4")
+        self.element_value_string5 = mommy.make("element_value.ElementValueString",
+                                                element_type_datum_object=self.element_type_datum_object5,
+                                                element_value="Test Object5")
+        self.element_value_string6 = mommy.make("element_value.ElementValueString",
+                                                element_type_datum_object=self.element_type_datum_object6,
+                                                element_value="Test Object6")
+
+
+        # Adjacent Associations
+        self.adjacent_association1 = mommy.make("association.AssociationAdjacent",
+                                                parent_datum=self.datum_object1,
+                                                child_datum=self.datum_object2
+                                                )
+        self.adjacent_association2 = mommy.make("association.AssociationAdjacent",
+                                                parent_datum=self.datum_object2,
+                                                child_datum=self.datum_object3
+                                                )
+        self.adjacent_association3 = mommy.make("association.AssociationAdjacent",
+                                                parent_datum=self.datum_object1,
+                                                child_datum=self.datum_object3
+                                                )
 
