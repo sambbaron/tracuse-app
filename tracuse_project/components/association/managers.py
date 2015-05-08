5266b023b27f69bdcec9095dbe646ca32190dae4
Sam Baron <sambaron@ownmail.net>
2015-05-08 11:34:47 -0700

    Revise association custom manager/queryset
    Fix manager call of queryset to include args.
    Move defaults to from queryset to manager.
    Tests: Pass

--- tracuse_project/components/association/managers.py
+++ tracuse_project/components/association/managers.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class AssociationQuerySet(models.QuerySet):
 
-    def filter_distance(self, distance_limit=1):
+    def filter_distance(self, distance_limit):
         if distance_limit:
             distance_filter = models.Q(distance__lte=distance_limit)
             return self.filter(distance_filter)
         else:
             return self
 
-    def exclude_self(self, exclude_self=False):
+    def exclude_self(self, exclude_self):
         if exclude_self:
             self_exclude = models.Q(parent_datum=models.F("child_datum"))
             return self.exclude(self_exclude)
@@ -24,8 +24,8 @@ class AssociationManager(models.Manager):
     def get_queryset(self):
         return AssociationQuerySet(self.model, using=self._db)
 
-    def filter_distance(self):
-        return self.get_queryset().filter_distance()
+    def filter_distance(self, distance_limit=1):
+        return self.get_queryset().filter_distance(distance_limit)
 
-    def exclude_self(self):
-        return self.get_queryset().exclude_self()
+    def exclude_self(self, exclude_self=True):
+        return self.get_queryset().exclude_self(exclude_self)
