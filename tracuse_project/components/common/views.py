d1c25f7f01fd718726e0a8d1724401590e564d11
Sam Baron <sambaron@ownmail.net>
2015-05-10 14:52:04 -0700

    Testing simple view and url

--- tracuse_project/components/common/views.py
+++ tracuse_project/components/common/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+
+from components.datum.models import DatumObject
+
+def index(request):
+
+    datum = DatumObject.objects.first()
+    datum_dict = datum.as_dict_all
+    datum_json = json.dumps(datum_dict)
+    response = HttpResponse(datum_json)
+    return response
+
