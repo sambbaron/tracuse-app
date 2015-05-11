9274b5f0e19755574df09424c570f24bbc2ea26a
Sam Baron <sambaron@ownmail.net>
2015-05-11 14:34:04 -0700

    Setup assets and templates folder
    Remove __init__, add assets sub-folders

--- tracuse_project/config/settings.py
+++ tracuse_project/config/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 # Custom data serializers
 SERIALIZATION_MODULES = {
