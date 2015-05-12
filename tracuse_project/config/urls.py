b9f1fae4f23b55b62109a813c314d029492db4ff
Sam Baron <sambaron@ownmail.net>
2015-05-12 09:48:57 -0700

    Fix initial urls regex for multiple digits

--- tracuse_project/config/urls.py
+++ tracuse_project/config/urls.py
@@ -6,8 +6,8 @@ from website import views
 urlpatterns = [
 
     url(r'^$', views.datums_get, name='datums_get'),
-    url(r'^datum/(?P<datum_pk>[0-9])/update/$', views.datums_post, name='datums_post'),
-    url(r'^datum/(?P<datum_pk>[0-9])/delete/$', views.datums_delete, name='datums_delete'),
+    url(r'^datum/(?P<datum_pk>[0-9]*)/update/$', views.datums_post, name='datums_post'),
+    url(r'^datum/(?P<datum_pk>[0-9]*)/delete/$', views.datums_delete, name='datums_delete'),
     url(r'^datum/add/$', views.datums_add, name='datums_add'),
 
     url(r'^admin/', include(admin.site.urls)),
