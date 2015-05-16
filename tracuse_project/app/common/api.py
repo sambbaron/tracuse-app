1dd303b207754b422d0c53c5360e8433e7743f75
Sam Baron <sambaron@ownmail.net>
2015-05-16 14:01:17 -0700

    Add new API endpoints to common/api.py
    Untested

--- tracuse_project/app/common/api.py
+++ tracuse_project/app/common/api.py
@@ -2,12 +2,12 @@
 
 from django.conf.urls import url
 
-from . import views
+from app.datum.views import DatumObjectAll, DatumObjectOne
 
 urlpatterns = [
 
-    url(r'^datum_groups/$', views.datum_groups_get),
-    url(r'^datum_types/$', views.datum_types_get),
-    url(r'^datum_objects/$', views.datum_objects_get),
+    #API Endpoints
+    url(r'^datum_objects/$', DatumObjectAll.as_view(), name="datum-object-all"),
+    url(r'^datum_object/(?P<pk>[0-9]+)/$', DatumObjectOne.as_view(), name="datum-object-one"),
 
 ]
