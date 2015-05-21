a0949bcecfbaeb4d51d70a9126bf466e3fd65949
Sam Baron <sambaron@ownmail.net>
2015-05-20 21:00:38 -0700

    Add AssociationAdjacent model to admin

--- tracuse_project/app/association/admin.py
+++ tracuse_project/app/association/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AssociationAdjacent
+
+
+@admin.register(AssociationAdjacent)
+class AssociationAdjacentAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "parent_datum", "child_datum", "association_type")
+    list_editable = ("parent_datum", "child_datum", "association_type")
+
+    fields = ("parent_datum", "child_datum", "association_type")
