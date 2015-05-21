b1cff038f928f252a26d4eca10ad2537e96c0edc
Sam Baron <sambaron@ownmail.net>
2015-05-20 21:28:00 -0700

    Add AssociationAll to admin

--- tracuse_project/app/association/admin.py
+++ tracuse_project/app/association/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import AssociationAdjacent
+from .models import AssociationAdjacent, AssociationAll
 
 
 @admin.register(AssociationAdjacent)
@@ -9,3 +9,7 @@ class AssociationAdjacentAdmin(admin.ModelAdmin):
     list_editable = ("parent_datum", "child_datum", "association_type")
 
     fields = ("parent_datum", "child_datum", "association_type")
+
+@admin.register(AssociationAll)
+class AssociationAllAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "parent_datum", "child_datum", "distance")
