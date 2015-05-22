4a7c0fccb3c3698932ba75bfb73c317e72b32934
Sam Baron <sambaron@ownmail.net>
2015-05-22 14:33:37 -0700

    Create batch of filter tests
    Covers all rules.
    Tests: Pass

--- tracuse_project/app/filter/tests/test_data_filters.py
+++ tracuse_project/app/filter/tests/test_data_filters.py
@@ -9,54 +9,108 @@ from .. import models as filters
 
 
 class TestFilterSets(object):
+
     def __init__(self):
-        self.filter_set1 = {
+
+        self.filter_set_user1 = {
             "filter_set_user_rules": [
                 filters.FilterRuleUser(
                     user=User.objects.get(pk=1),
                 ),
             ],
+        }
+
+        self.filter_set_group1 = {
             "filter_set_group_rules": [
                 filters.FilterRuleGroup(
                     datum_group=DatumGroup.objects.get(entity_name="Area"),
                 ),
+            ],
+        }
+
+        self.filter_set_group2 = {
+            "filter_set_group_rules": [
                 filters.FilterRuleGroup(
-                    conditional="OR",
                     datum_group=DatumGroup.objects.get(entity_name="Activity"),
                 ),
+                filters.FilterRuleGroup(
+                    conditional="OR",
+                    datum_group=DatumGroup.objects.get(entity_name="Item"),
+                ),
             ],
+        }
+
+        self.filter_set_type1 = {
+            "filter_set_type_rules": [
+                filters.FilterRuleType(
+                    datum_type=DatumType.objects.get(entity_name="Context"),
+                ),
+            ],
+        }
+        
+        self.filter_set_type2 = {
             "filter_set_type_rules": [
                 filters.FilterRuleType(
                     datum_type=DatumType.objects.get(entity_name="Category"),
                 ),
                 filters.FilterRuleType(
-                    conditional="OR",
-                    datum_type=DatumType.objects.get(entity_name="Action"),
+                    conditional="AND",
+                    datum_type=DatumType.objects.get(entity_name="Event"),
                 ),
             ],
+        }
+
+        self.filter_set_association1 = {
             "filter_set_association_rules": [
                 filters.FilterRuleAssociation(
                     datum_object=DatumObject.objects.get(pk=3),
-                    association_direction=AssociationDirection.both()
-                ),
-                filters.FilterRuleAssociation(
-                    conditional="OR",
-                    datum_object=DatumObject.objects.get(pk=10),
-                    association_direction=AssociationDirection.both(),
+                    association_direction=AssociationDirection.child(),
+                    distance=None
                 ),
             ],
+        }
+
+        self.filter_set_association2 = {
+            "filter_set_association_rules": [
+                filters.FilterRuleAssociation(
+                    datum_object=DatumObject.objects.get(pk=14),
+                    association_direction=AssociationDirection.parent(),
+                    distance=2
+                ),
+            ],
+        }
+
+        self.filter_set_element1 = {
             "filter_set_element_rules": [
                 filters.FilterRuleElement(
                     element_type=ElementType.objects.get(entity_name="Name"),
                     operator="icontains",
-                    elvalue="pyth",
-                ),
-                filters.FilterRuleElement(
-                    conditional="OR",
-                    element_type=ElementType.objects.get(entity_name="Description"),
-                    operator="icontains",
-                    elvalue="apart",
+                    elvalue="pdx code guild",
                 ),
             ],
+        }
 
+        self.filter_set_element2 = {
+            "filter_set_element_rules": [
+                filters.FilterRuleElement(
+                    element_type=ElementType.objects.get(entity_name="ActionEffort"),
+                    operator="exact",
+                    elvalue="4",
+                ),
+            ],
+        }
+
+        self.filter_set_element3 = {
+            "filter_set_element_rules": [
+                filters.FilterRuleElement(
+                    element_type=ElementType.objects.get(entity_name="ActionEffort"),
+                    operator="exact",
+                    elvalue="3",
+                ),
+                filters.FilterRuleElement(
+                    element_type=ElementType.objects.get(entity_name="Description"),
+                    operator="icontains",
+                    elvalue="craigs",
+                ),
+            ],
         }
