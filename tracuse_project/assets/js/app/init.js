e6a0b8919c76ec3da02bda58798c2cb128e26c31
Sam Baron <sambaron@ownmail.net>
2015-05-23 12:04:47 -0700

    Add render testing to init

--- tracuse_project/assets/js/app/init.js
+++ tracuse_project/assets/js/app/init.js
@@ -8,6 +8,15 @@ Tracuse.init = function init() {
     Tracuse.models.createModels();
     Tracuse.models.loadInitData();
     Tracuse.templates.loadEnvironment();
+
+    var render_button = document.querySelector("#render-page");
+    render_button.addEventListener("click", function () {
+
+        // Test custom element
+        var dom = new Tracuse.elements.DatumObject(12);
+        dom.innerHTML = dom.headline;
+        Tracuse.frame.appendChild(dom);
+    });
 };
 
 document.addEventListener("DOMContentLoaded", function () {
