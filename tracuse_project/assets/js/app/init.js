c8362cdb7ddf8acef4be1350c46d5c16a58458be
Sam Baron <sambaron@ownmail.net>
2015-05-24 09:48:53 -0700

    Revise client-side templates
    Viewuse, datum, element.
    Recreate attributes from web components.
    Successfully inherit.

--- tracuse_project/assets/js/app/init.js
+++ tracuse_project/assets/js/app/init.js
@@ -12,13 +12,20 @@ Tracuse.init = function init() {
     var renderButton = document.querySelector("#render-page");
     renderButton.addEventListener("click", function () {
 
-        var datumIds = [1, 3, 8];
+        var datumIds = [1, 3, 8, 13];
         var datumObjects = Tracuse.models.idsToObjects(
             Tracuse.models.datum_objects,
             datumIds
         );
-        var datumHtmlEl = Tracuse.views.datums(datumObjects);
-        Tracuse.frame.appendChild(datumHtmlEl);
+
+        var templateData = {
+            "datumTemplate": "datum_medium",
+            "datums": datumObjects
+        };
+
+        //var datumHtmlEl = Tracuse.views.datums(datumObjects);
+        var rendered = Tracuse.templates.render("viewuse/viewuse_basic.html", templateData);
+        Tracuse.frame.innerHTML = rendered;
     });
 };
 
