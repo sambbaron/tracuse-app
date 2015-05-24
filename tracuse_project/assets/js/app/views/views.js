ca18f93c8d710a0a2337ad34cab8b1d667dcf697
Sam Baron <sambaron@ownmail.net>
2015-05-23 23:13:27 -0700

    Create views.datums - renders array of datum objects

--- tracuse_project/assets/js/app/views/views.js
+++ tracuse_project/assets/js/app/views/views.js
@@ -2,3 +2,27 @@ var Tracuse = Tracuse || {};
 
 // Views collection
 Tracuse.views = Tracuse.views || {};
+
+
+Tracuse.views.datums = function datums(datumObjects) {
+    // Render array of datum objects to node
+    // Arguments:
+    //      datumObjects: Array of datum objects without nested ids
+    "use strict";
+    var outputFragment = document.createDocumentFragment();
+    var datumModel = Tracuse.models.datum_objects;
+    var elementModel = Tracuse.models.element_datum_objects;
+
+    for (var i = 0, max = datumObjects.length; i < max; i++) {
+        var datumObject = datumObjects[i];
+        var datumId = datumObject[datumModel.idProperty];
+        var datumHtmlEl = new Tracuse.elements.DatumObject(datumId);
+
+        for (var elementId in datumObject.elements) {
+            var element = new Tracuse.elements.DatumElement(elementId, datumId);
+            datumHtmlEl.appendChild(element);
+        }
+        outputFragment.appendChild(datumHtmlEl);
+    }
+    return outputFragment
+};
