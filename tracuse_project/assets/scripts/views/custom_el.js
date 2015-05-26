b5a410cb46ecb90cf039b52ebef8170804c06e7f
Sam Baron <sambaron@ownmail.net>
2015-05-26 14:41:13 -0700

    Viewuse buttons only show on hover
    Doesn't work properly for nested viewuse.

--- tracuse_project/assets/scripts/views/custom_el.js
+++ tracuse_project/assets/scripts/views/custom_el.js
@@ -38,7 +38,28 @@ Tracuse.customEl.Input = document.registerElement(
 
 Tracuse.customEl.Viewuse = document.registerElement(
     "x-viewuse",
-    {prototype: Object.create(HTMLElement.prototype)}
+    {
+        prototype: Object.create(HTMLElement.prototype, {
+            createdCallback: {
+                value: function () {
+                    this.addEventListener("mouseenter", function (e) {
+                        elements = e.target.querySelectorAll("x-viewuse > button");
+                        for (var i = 0; i < elements.length; i++) {
+                            elements[i].style.display = "initial";
+                        }
+                        e.stopPropagation();
+                    });
+                    this.addEventListener("mouseleave", function (e) {
+                        var elements = e.target.querySelectorAll("x-viewuse > button");
+                        for (var i = 0; i < elements.length; i++) {
+                            elements[i].style.display = "none";
+                        }
+                        e.stopPropagation();
+                    });
+                }
+            }
+        })
+    }
 );
 
 Tracuse.customEl.ViewuseButton = document.registerElement(
@@ -48,6 +69,8 @@ Tracuse.customEl.ViewuseButton = document.registerElement(
         prototype: Object.create(HTMLElement.prototype, {
             createdCallback: {
                 value: function () {
+                    this.style.display = "none";
+
                     this.addEventListener("mouseenter", function (e) {
                         e.target.querySelector("span").style.display = "none";
                     });
