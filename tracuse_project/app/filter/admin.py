a8a0301bb1055275339ea4fbbb9cd82eb49796db
Sam Baron <sambaron@ownmail.net>
2015-05-30 12:06:57 -0700

    Add FilterSet model admin

--- tracuse_project/app/filter/admin.py
+++ tracuse_project/app/filter/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.common.admin import BaseModelAdmin, BaseModelInline, EntityModelAdmin, EntityModelInline
+from .models import FilterSet
+
+
+class FilterSetInline(EntityModelInline):
+    model = FilterSet
+
+    fields = EntityModelInline.fields
+
+
+@admin.register(FilterSet)
+class FilterSetAdmin(EntityModelAdmin):
+    list_display = EntityModelAdmin.list_display
+    list_editable = EntityModelAdmin.list_editable
+
+    fields = EntityModelAdmin.fields
