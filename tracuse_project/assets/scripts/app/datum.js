7ce1ba77f5aaad4df0fd670b00ac19c405261f46
Sam Baron <sambaron@ownmail.net>
2015-06-01 11:13:51 -0700

    Fix extendElements to accomodate empty datum objects

--- tracuse_project/assets/scripts/app/datum.js
+++ tracuse_project/assets/scripts/app/datum.js
@@ -7,17 +7,20 @@ Tracuse.app.datum = Tracuse.app.datum || {};
 Tracuse.app.datum.extendElements = function extendElements(datumObjects, callback) {
     "use strict";
     // Extend datum objects with element objects
+
     var property = Tracuse.models.datum_objects.properties[7];
-    var c = 0;
-    for (var i = 0, imax = datumObjects.length; i < imax; i++) {
-        var datumObject = datumObjects[i];
-        Tracuse.models.extendObject(datumObject, property, function(extendedObject) {
-            datumObjects[i] = extendedObject;
-            c++
-        });
+    var c = 0, i = 0;
+    if (datumObjects) {
+        for (var imax = datumObjects.length; i < imax; i++) {
+            var datumObject = datumObjects[i];
+            Tracuse.models.extendObject(datumObject, property, function (extendedObject) {
+                datumObjects[i] = extendedObject;
+                c++
+            });
+        }
     }
 
-    var checkObject = setInterval(function() {
+    var checkObject = setInterval(function () {
         if (c === i) {
             clearInterval(checkObject);
             callback(datumObjects);
