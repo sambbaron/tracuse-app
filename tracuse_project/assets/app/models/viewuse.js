d9a1e1bc66ed49c93339138448d92fd737ad4f09
Sam Baron <sambaron@ownmail.net>
2015-06-12 15:02:38 -0700

    Add relationships to ViewuseObject model
    ViewuseArrangement and ViewuseDatum

--- tracuse_project/assets/app/models/viewuse.js
+++ tracuse_project/assets/app/models/viewuse.js
@@ -3,6 +3,18 @@ Tracuse.models.ViewuseObject =
     Tracuse.models.ModelFactory("viewuse_object", "viewuse_object_id");
 Tracuse.models.ViewuseObject.prototype.relations = [
     {
+        type: Backbone.HasOne,
+        key: "viewuse_arrangement_id",
+        relatedModel: "Tracuse.models.ViewuseArrangement",
+        collectionType: "Tracuse.models.ViewuseArrangement.collBase"
+    },
+    {
+        type: Backbone.HasOne,
+        key: "viewuse_datum_id",
+        relatedModel: "Tracuse.models.ViewuseDatum",
+        collectionType: "Tracuse.models.ViewuseDatum.collBase"
+    },
+    {
         type: Backbone.HasMany,
         key: "filters",
         relatedModel: "Tracuse.models.ViewuseFilter",
