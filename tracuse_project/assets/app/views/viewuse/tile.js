159976bf50e9366507d36276eec1263a68ef70de
Sam Baron <sambaron@ownmail.net>
2015-06-12 17:04:05 -0700

    Fix datumObject and append element pass through from viewuse to datum

--- tracuse_project/assets/app/views/viewuse/tile.js
+++ tracuse_project/assets/app/views/viewuse/tile.js
@@ -7,17 +7,24 @@ Tracuse.views.ViewuseTile = Tracuse.views.ViewuseBase.extend({
         Tracuse.views.ViewuseBase.prototype.render.apply(viewuseView, arguments);
 
         // Add class for arrangement view name
-        var viewName = viewuseView.model.get("viewuse_arrangement_id").get("schema_name");
-        viewuseView.el.classList.add(viewName);
+        viewuseView.el.classList.add("viewuse_tile");
 
-        var datumView = viewuseView.model.get("viewuse_datum_id").get("entity_name");
+        // Get datum view class name
+        var datumViewName = viewuseView.model.get("viewuse_datum_id").get("entity_name");
+
+        // Set element to append datums
+        var appendEl = viewuseView.el.querySelector(".content");
 
         // Return filtered datums
         var filter = viewuseView.model.get("filters").first().attributes;
         Tracuse.utils.getFilteredDatums(filter, function (datumObjects) {
 
             for (var i = 0, imax = datumObjects.length; i < imax; i++) {
-                var datumObject = datumObjects[i];
+                var datumObject = datumObjects.models[i];
+                new Tracuse.views[datumViewName]({
+                    model: datumObject,
+                    appendEl: appendEl
+                });
             }
 
         });
