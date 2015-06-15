e0218419d6de2731cd7f9fa90d29a475244e95f4
Sam Baron <sambaron@ownmail.net>
2015-06-15 11:30:56 -0700

    Reimplement DatumBase template and render
    Use for icons.

--- tracuse_project/assets/app/views/datum/base.js
+++ tracuse_project/assets/app/views/datum/base.js
@@ -1,6 +1,30 @@
 Tracuse.views.DatumBase = Backbone.View.extend({
 
     tagName: "article",
-    className: "datum"
+    className: "datum",
+    templateName: "datum/base.html",
+
+    template: function () {
+        "use strict";
+        var datumView = this;
+        var templateOutput = "";
+
+        var templateData = {
+            this_viewuse: datumView.model.toJSON()
+        };
+        templateOutput = Tracuse.templates.env.render(
+            datumView.templateName,
+            templateData
+        );
+
+        return templateOutput;
+    },
+
+    render: function () {
+        "use strict";
+        var datumView = this;
+        datumView.el.innerHTML = datumView.template();
+        return datumView;
+    }
 
 });
