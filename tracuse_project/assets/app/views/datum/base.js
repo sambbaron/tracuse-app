7cb17a25047cb12e6cdffdf94fa5ba5a8c2c3e8d
Sam Baron <sambaron@ownmail.net>
2015-06-15 17:10:46 -0700

    Add datum group and datum type names as classes on datum element
    Use for css styling.

--- tracuse_project/assets/app/views/datum/base.js
+++ tracuse_project/assets/app/views/datum/base.js
@@ -9,6 +9,11 @@ Tracuse.views.DatumBase = Backbone.View.extend({
         var datumView = this;
         var templateOutput = "";
 
+        // Add Datum Group and Datum Type names to class
+        // Using names rather than ids to be more transparent, but names are a dependency
+        datumView.el.classList.add(datumView.model.get("datum_group_id").get("schema_name"));
+        datumView.el.classList.add(datumView.model.get("datum_type_id").get("schema_name"));
+
         var templateData = {
             this_datum: datumView.model.toJSON()
         };
