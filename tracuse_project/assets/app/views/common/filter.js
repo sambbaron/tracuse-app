7c88987d19a6ec97df1d3be6c0aed3db22b8161f
Sam Baron <sambaron@ownmail.net>
2015-06-17 11:06:22 -0700

    Add events to select filter elements
    Not completely working. Filter on related object.

--- tracuse_project/assets/app/views/common/filter.js
+++ tracuse_project/assets/app/views/common/filter.js
@@ -8,6 +8,22 @@ Tracuse.views.DatumFilter = Backbone.View.extend({
         "drag": function drag(ev, ui) {
             this.el.classList.add("drag");
             ev.stopPropagation();
+        },
+        "change .associations select[name='association_groups']": function (ev) {
+            this.selectAssociationGroup(ev.target);
+            ev.stopPropagation();
+        },
+        "change .associations select[name='association_types']": function (ev) {
+            this.selectAssociationType(ev.target);
+            ev.stopPropagation();
+        },
+        "change .elements select[name='element_types']": function (ev) {
+            this.selectElement(ev.target);
+            ev.stopPropagation();
+        },
+        "click button[name^='add-']": function (ev) {
+            this.addFilter(ev.target);
+            ev.stopPropagation();
         }
     },
 
@@ -52,6 +68,82 @@ Tracuse.views.DatumFilter = Backbone.View.extend({
     showHide: function showHide() {
         "use strict";
         this.$el.fadeToggle(200);
+    },
+
+    selectAssociationGroup: function selectAssociationGroup(el) {
+        "use strict";
+        /* Filter Datum Types*/
+        var filterEl = this.el;
+        var optionFrag, optionEl;
+
+        // Update Datum Types
+        var datumTypesEl = filterEl.querySelector("[name='association_types']");
+        datumTypesEl.innerHTML = "";
+        optionFrag = document.createDocumentFragment();
+        var datumGroup = Tracuse.models.DatumGroup.all.get(el.value);
+        var datumTypes = datumGroup.get("datum_types").models;
+        _.each(datumTypes, function (datumType) {
+            optionEl = document.createElement("option");
+            optionEl.innerHTML = datumType.get("readable_name");
+            optionEl.value = datumType.id;
+            optionFrag.appendChild(optionEl);
+        });
+        datumTypesEl.appendChild(optionFrag);
+
+        // Update Datum Objects
+        var datumObjectsEl = filterEl.querySelector("[name='association_datums']");
+        datumObjectsEl.innerHTML = "";
+
+        optionFrag = document.createDocumentFragment();
+        var datumObjects = Tracuse.models.DatumObject.all.where({
+            datum_group_id: parseInt(el.value)
+        });
+        _.each(datumObjects, function (datum) {
+            optionEl = document.createElement("option");
+            optionEl.innerHTML = datum.get("headline");
+            optionEl.value = datum.id;
+            optionFrag.appendChild(optionEl);
+        });
+        datumObjectsEl.appendChild(optionFrag);
+    },
+
+    selectAssociationType: function selectAssociationType(el) {
+        "use strict";
+        /* Update Datum Objects*/
+        var filterEl = this.el;
+        var datumObjectsEl = filterEl.querySelector("[name='association_datums']");
+        datumObjectsEl.innerHTML = "";
+
+        var optionFrag = document.createDocumentFragment();
+        var datumObjects = Tracuse.models.DatumObject.all.where({
+            datum_type_id: parseInt(el.value)
+        });
+        _.each(datumObjects, function(datum){
+            var optionEl = document.createElement("option");
+            optionEl.innerHTML = datum.get("headline");
+            optionEl.value = datum.id;
+            optionFrag.appendChild(optionEl);
+        });
+        datumObjectsEl.appendChild(optionFrag);
+    },
+
+    selectElement: function selectElement(el) {
+        // Add element operators
+        var filterEl = this.el;
+        var elementOperatorsEl = filterEl.querySelector("[name='element_operators']");
+        elementOperatorsEl.innerHTML = "";
+
+        var elementType = Tracuse.models.ElementType.all.get(el.value);
+        var elementOperators = elementType.get("element_operators").models;
+
+        var optionFrag = document.createDocumentFragment();
+        _.each(elementOperators, function(operator) {
+            var optionEl = document.createElement("option");
+            optionEl.innerHTML = operator.get("readable_name");
+            optionEl.value = operator.get("entity_name");
+            optionFrag.appendChild(optionEl);
+        });
+        elementOperatorsEl.appendChild(optionFrag);
     }
 
 });
