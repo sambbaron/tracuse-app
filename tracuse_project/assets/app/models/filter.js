866842218b718d8132db73b2a33c998b937316e4
Sam Baron <sambaron@ownmail.net>
2015-06-18 13:35:24 -0700

    Add relations to FilterRule models
    Will create FilterRule views and templates as well

--- tracuse_project/assets/app/models/filter.js
+++ tracuse_project/assets/app/models/filter.js
@@ -1,8 +1,71 @@
 Tracuse.models.FilterRuleUser = Backbone.RelationalModel.extend({});
-Tracuse.models.FilterRuleGroup = Backbone.RelationalModel.extend({});
-Tracuse.models.FilterRuleType = Backbone.RelationalModel.extend({});
-Tracuse.models.FilterRuleAssociation = Backbone.RelationalModel.extend({});
-Tracuse.models.FilterRuleElement = Backbone.RelationalModel.extend({});
+
+Tracuse.models.FilterRuleGroup = Backbone.RelationalModel.extend({
+    relations: [
+        {
+            type: Backbone.HasOne,
+            key: "datum_group",
+            relatedModel: "Tracuse.models.DatumGroup"
+        }
+    ],
+
+    initialize: function (options) {
+        "use strict";
+        this.set("datum_group", options.datum_group_id);
+    }
+});
+
+Tracuse.models.FilterRuleType = Backbone.RelationalModel.extend({
+    relations: [
+        {
+            type: Backbone.HasOne,
+            key: "datum_type",
+            relatedModel: "Tracuse.models.DatumType"
+        }
+    ],
+
+    initialize: function (options) {
+        "use strict";
+        this.set("datum_type", options.datum_type_id);
+    }
+});
+
+Tracuse.models.FilterRuleAssociation = Backbone.RelationalModel.extend({
+    relations: [
+        {
+            object: Backbone.HasOne,
+            key: "datum_object",
+            relatedModel: "Tracuse.models.DatumObject"
+        }
+    ],
+
+    initialize: function (options) {
+        "use strict";
+        this.set("datum_object", options.datum_object_id);
+    }
+});
+
+Tracuse.models.FilterRuleElement = Backbone.RelationalModel.extend({
+    relations: [
+        {
+            object: Backbone.HasOne,
+            key: "element_type",
+            relatedModel: "Tracuse.models.ElementType"
+        },
+        {
+            object: Backbone.HasOne,
+            key: "element_operator",
+            relatedModel: "Tracuse.models.ElementOperator"
+        }
+    ],
+
+    initialize: function (options) {
+        "use strict";
+        this.set("element_type", options.element_type_id);
+        this.set("element_operator", options.element_operator_id);
+    }
+});
+
 Tracuse.models.FilterRuleDataType = Backbone.RelationalModel.extend({});
 
 Tracuse.models.FilterSet = Backbone.RelationalModel.extend({
