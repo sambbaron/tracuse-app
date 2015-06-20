265b39143584a5ab12d662d293ba08c5625b774e
Sam Baron <sambaron@ownmail.net>
2015-06-20 12:53:07 -0700

    Refactor update model (put/post) into separate functions
    update_prep, update_save, update_response
    Allow for customization with specific views.
    Need this for update element.

--- tracuse_project/app/utils/view.py
+++ tracuse_project/app/utils/view.py
@@ -63,6 +63,27 @@ class ViewOne(ViewBase):
     post_field_list = field_list
     put_field_list = field_list
 
+    def update_prep(self, request):
+        """Convert request data to python object"""
+        request_data = request.body.decode()
+        return json.loads(request_data, encoding=DjangoJSONEncoder)
+
+    def update_save(self, model_object, request_data):
+        """Save request data to model object"""
+        return update_model(model_object, self.post_field_list, request_data)
+
+    def update_response(self, save_result, success_code, fail_code):
+        """Return HTTP response for data update
+        Assume that string response is an error
+        """
+        if type(save_result) == str:
+            response = JsonResponse(save_result, status=fail_code, safe=False)
+        else:
+            response_data = self.serialized_data(save_result)
+            response = JsonResponse(response_data, status=success_code)
+
+        return response
+
     def get(self, request, pk):
         object = self.get_object(pk)
         response_data = self.serialized_data(object)
@@ -71,30 +92,16 @@ class ViewOne(ViewBase):
 
     def post(self, request, pk):
         object = self.model.objects.create()
-        request_data = request.body.decode()
-        serialized_data = json.loads(request_data, encoding=DjangoJSONEncoder)
-
-        model_save = update_model(object, self.post_field_list, serialized_data)
-        if type(model_save) == str:
-            response = JsonResponse(model_save, status=400, safe=False)
-        else:
-            response_data = self.serialized_data(model_save)
-            response = JsonResponse(response_data, status=201)
-
+        request_data = self.update_prep(request)
+        save_result = self.update_save(object, request_data)
+        response = self.update_response(save_result, 201, 400)
         return response
 
     def put(self, request, pk):
         object = self.get_object(pk)
-        request_data = request.body.decode()
-        serialized_data = json.loads(request_data, encoding=DjangoJSONEncoder)
-
-        model_save = update_model(object, self.put_field_list, serialized_data)
-        if type(model_save) == str:
-            response = JsonResponse(model_save, status=400, safe=False)
-        else:
-            response_data = self.serialized_data(model_save)
-            response = JsonResponse(response_data, status=200)
-
+        request_data = self.update_prep(request)
+        save_result = self.update_save(object, request_data)
+        response = self.update_response(save_result, 200, 400)
         return response
 
     def delete(self, request, pk):
