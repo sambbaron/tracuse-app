7fa24918dd584170543e220ec9011a994848c8f9
Sam Baron <sambaron@ownmail.net>
2015-06-20 08:16:40 -0700

    Create tests for utils.model.update_model
    No test for general failure.
    Tests: Pass

--- tracuse_project/app/common/utils/tests.py
+++ tracuse_project/app/common/utils/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from model_mommy import mommy
 
 from . import entity
 from . import model
@@ -58,6 +59,15 @@ class TestUtilsEntity(TestCase):
 class TestUtilsModel(TestCase):
     """Test model functions"""
 
+    def setUp(self):
+        self.test_model = mommy.make("datum.DatumGroup",
+                                     entity_name="TestDatumGroup",
+                                     sort=10
+                                     )
+
+    def tearDown(self):
+        self.test_model.delete()
+
     def test_convert_field_data_boolean_false(self):
         """Test convert_field_data with boolean value
         """
@@ -102,3 +112,51 @@ class TestUtilsModel(TestCase):
         expected = '{"Test": "Data"}'
         actual = model.convert_field_data(test_data, test_type)
         self.assertEqual(expected, actual)
+
+    def test_update_model_success_with_type(self):
+        """Test update_model with explicit field data type
+        """
+        field_list = [("sort", "integer")]
+        data = {"sort": 99}
+        actual = model.update_model(self.test_model, field_list, data)
+        self.assertEqual(self.test_model, actual)
+        self.assertEqual(self.test_model.sort, 99)
+
+    def test_update_model_success_without_type(self):
+        """Test update_model with implicit field data type
+        """
+        field_list = [("sort",)]
+        data = {"sort": 99}
+        actual = model.update_model(self.test_model, field_list, data)
+        self.assertEqual(self.test_model, actual)
+        self.assertEqual(self.test_model.sort, 99)
+
+    def test_update_model_failure_bad_field(self):
+        """Test update_model with field not in model
+        """
+        field_list = [("badfield",)]
+        data = {"sort": 99}
+        expected = "'badfield' not a valid field"
+        actual = model.update_model(self.test_model, field_list, data)
+        self.assertEqual(expected, actual)
+        self.assertEqual(self.test_model.sort, 10)
+
+    def test_update_model_failure_field_not_data(self):
+        """Test update_model with field not in data
+        """
+        field_list = [("sort",)]
+        data = {"badfield": 99}
+        expected = "'sort' not in data request"
+        actual = model.update_model(self.test_model, field_list, data)
+        self.assertEqual(expected, actual)
+        self.assertEqual(self.test_model.sort, 10)
+
+    def test_update_model_failure_data_conversion(self):
+        """Test update_model with bad field data conversion
+        """
+        field_list = [("sort", "integer")]
+        data = {"sort": "abc"}
+        expected = "'integer' data conversion failure; Field: sort; Raw data: abc;"
+        actual = model.update_model(self.test_model, field_list, data)
+        self.assertEqual(expected, actual)
+        self.assertEqual(self.test_model.sort, 10)
