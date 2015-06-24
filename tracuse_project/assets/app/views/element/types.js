0489106848d3ddcfc10c52196a30dae507be09e9
Sam Baron <sambaron@ownmail.net>
2015-06-24 13:40:56 -0700

    Create ElementSelect view and template
    For basic drop-down.
    Also create separate element/label.html template and include
      in other element templates.

--- tracuse_project/assets/app/views/element/types.js
+++ tracuse_project/assets/app/views/element/types.js
@@ -1,5 +1,8 @@
 Tracuse.views.ElementText = Tracuse.views.ElementBase.extend({
-
     templateName: "element/text.html"
-
 });
+
+Tracuse.views.ElementSelect = Tracuse.views.ElementBase.extend({
+    templateName: "element/select.html"
+});
+
