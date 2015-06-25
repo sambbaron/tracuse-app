e8df5d7085c559ab8855e02ba072018373aaf398
Sam Baron <sambaron@ownmail.net>
2015-06-25 13:31:41 -0700

    Change element class to base element.

--- tracuse_project/assets/app/views/element/types.js
+++ tracuse_project/assets/app/views/element/types.js
@@ -1,8 +1,10 @@
 Tracuse.views.ElementText = Tracuse.views.ElementBase.extend({
+    className: "element element-text",
     templateName: "element/text.html"
 });
 
 Tracuse.views.ElementSelect = Tracuse.views.ElementBase.extend({
+    className: "element element-select",
     templateName: "element/select.html"
 });
 
