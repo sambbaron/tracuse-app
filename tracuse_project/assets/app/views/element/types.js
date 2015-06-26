515bf17baf5119f3c27f13d4e8e80b36d631960a
Sam Baron <sambaron@ownmail.net>
2015-06-25 21:46:28 -0700

    Create ElementDatetime view
    Use jquery-datetimepicker and moment objects.
    Still need to format datetimepicker.

--- tracuse_project/assets/app/views/element/types.js
+++ tracuse_project/assets/app/views/element/types.js
@@ -8,3 +8,27 @@ Tracuse.views.ElementSelect = Tracuse.views.ElementBase.extend({
     templateName: "element/select.html"
 });
 
+Tracuse.views.ElementDatetime = Tracuse.views.ElementBase.extend({
+    className: "element element-datetime",
+    templateName: "element/base.html",
+
+    render: function () {
+        "use strict";
+        var elementView = Tracuse.views.ElementBase.prototype.render.apply(this, arguments);
+
+        var rawDatetime = elementView.model.get("element_value");
+        var formatDatetime = rawDatetime.format("M/D/YYYY h:mm a");
+        var inputEl = elementView.el.querySelector("input");
+        inputEl.setAttribute("value", formatDatetime);
+
+        $(inputEl).datetimepicker();
+        return elementView;
+    },
+
+    updateElement: function (value) {
+        "use strict";
+        var datetime = moment(value);
+        Tracuse.views.ElementBase.prototype.updateElement.call(this, datetime);
+    }
+});
+
