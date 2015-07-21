2aed7d500e4d9a9f84bb0223bf8b62ddb2f22a50
Sam Baron <sambaron@ownmail.net>
2015-07-21 16:06:34 -0700

    Create UiObjectModel admin class

--- tracuse_project/app/ui_object/admin.py
+++ tracuse_project/app/ui_object/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.common.admin import BaseModelAdmin, BaseModelInline
+
+from .models import UiObjectModel
+
+
+class UiObjectModelInline(BaseModelInline):
+    model = UiObjectModel
+
+    fields = BaseModelInline.fields + ("title", "description", "ui_arrangement_type", "ui_formatting_type", "datum_filter")
+
+
+class UiObjectModelAdmin(BaseModelAdmin):
+    list_display = BaseModelAdmin.list_display + ("title", "description", "ui_arrangement_type", "ui_formatting_type", "datum_filter",)
+    list_editable = BaseModelAdmin.list_editable + ("title", "description", "ui_arrangement_type", "ui_formatting_type", "datum_filter",)
+
+    fields = BaseModelAdmin.fields + ("title", "description", "ui_arrangement_type", "ui_formatting_type", "datum_filter")
+    readonly_fields = BaseModelAdmin.readonly_fields
