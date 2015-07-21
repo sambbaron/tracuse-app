6d034eb93a450b3ccb1d8ff4d84ed1824c84c9f5
Sam Baron <sambaron@ownmail.net>
2015-07-21 16:40:37 -0700

    Create admin classes for ui_arrangement and ui_formatting models

--- tracuse_project/app/ui_formatting/admin.py
+++ tracuse_project/app/ui_formatting/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.common.admin import BaseModelAdmin, BaseModelInline, EntityModelAdmin, EntityModelInline
+from .models import UiFormattingType, UiFormattingGeneric
+
+
+class UiFormattingTypeInline(EntityModelInline):
+    model = UiFormattingType
+
+
+@admin.register(UiFormattingType)
+class UiFormattingTypeAdmin(EntityModelAdmin):
+    list_display = EntityModelAdmin.list_display
+    list_editable = EntityModelAdmin.list_editable
+
+    fields = EntityModelAdmin.fields
+    readonly_fields = EntityModelAdmin.readonly_fields
+
+
+class UiFormattingGenericInline(BaseModelInline):
+    model = UiFormattingGeneric
+
+    fields = BaseModelAdmin.fields
+
+
+@admin.register(UiFormattingGeneric)
+class UiFormattingGenericAdmin(BaseModelAdmin):
+    list_display = BaseModelAdmin.list_display
+    list_editable = BaseModelAdmin.list_editable
+
+    fields = BaseModelAdmin.fields
