a4099dfd4f90f6baea583933368e7e069bfd53b3
Sam Baron <sambaron@ownmail.net>
2015-07-21 19:05:38 -0700

    Update Viewuse and Windowuse serializer for UiObjectModelSerializer
    Tests: Pass

--- tracuse_project/app/viewuse/serializers.py
+++ tracuse_project/app/viewuse/serializers.py
@@ -1,5 +1,6 @@
-from .models import ViewuseObject
 from app.ui_object.serializers import UiObjectModelSerializer
+
+from .models import ViewuseObject
 
 
 class ViewuseObjectSerializer(UiObjectModelSerializer):
--- tracuse_project/app/windowuse/serializers.py
+++ tracuse_project/app/windowuse/serializers.py
@@ -1,31 +1,16 @@
-import json
-
 from app.utils.serializer import Serializer
+from app.ui_object.serializers import UiObjectModelSerializer
 
 from .models import WindowuseObject, WindowuseViewuse
 
 
-class WindowuseObjectSerializer(Serializer):
+class WindowuseObjectSerializer(UiObjectModelSerializer):
     model = WindowuseObject
 
-    def serial_default(self):
-        output = super().serial_default()
-        output.remove("datum_filter")
-        output.append(("datum_filter", json.loads(self.obj.datum_filter)))
-        return output
-
     def serial_related(self):
-        output = self.serial_default()
+        output = super().serial_default()
         output.append(("viewuse_objects", [viewuse.viewuse_object_id for viewuse in self.obj.windowuse_viewuses.all()]))
         return output
-
-    def serial_update(self):
-        return [
-            "title",
-            "description",
-            ("datum_filter", json.dumps(self.obj.datum_filter))
-        ]
-
 
 class WindowuseViewuseSerializer(Serializer):
     model = WindowuseViewuse
