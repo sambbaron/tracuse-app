982f9b16a15591af8fb79749309a6b737594e006
Sam Baron <sambaron@ownmail.net>
2015-07-29 19:59:58 -0700

    Remove render and initialize methods from DatumMedium
    Continue using on a limited basis.

--- tracuse_project/assets/app/views/datum/medium.js
+++ tracuse_project/assets/app/views/datum/medium.js
@@ -1,5 +1,7 @@
 Tracuse.views.DatumMedium = Tracuse.views.DatumBase.extend({
 
+    objectTypeClass: "datum datum-medium",
+
     events: {
         "focusin .element-input": function (ev) {
             "use strict";
@@ -11,28 +13,6 @@ Tracuse.views.DatumMedium = Tracuse.views.DatumBase.extend({
             this.setActiveElement(ev.target);
             ev.stopPropagation();
         }
-    },
-
-    render: function () {
-        "use strict";
-        var datumView = Tracuse.views.DatumBase.prototype.render.apply(this, arguments);
-
-        // Add class for arrangement view name
-        datumView.el.classList.add("datum-medium");
-
-        //// Render elements
-        //// Only add elements with ElementDatumType.primary_view set true
-        //var fragment = document.createDocumentFragment();
-        //_.each(datumView.elementSubViews, function (elementView) {
-        //    if (elementView.model.get("element_datum_type").get("primary_view")) {
-        //        fragment.appendChild(elementView.render().el);
-        //    }
-        //});
-        //datumView.el.appendChild(fragment);
-
-        return datumView;
-    },
-
-
+    }
 
 });
