5b66a856623797f9f6c07fb163d505624de5cfec
Sam Baron <sambaron@ownmail.net>
2015-07-29 20:33:07 -0700

    Remove events from DatumMedium
    Now empty except for class.
    Will re-add methods as needed.

--- tracuse_project/assets/app/views/datum/medium.js
+++ tracuse_project/assets/app/views/datum/medium.js
@@ -1,18 +1,5 @@
 Tracuse.views.DatumMedium = Tracuse.views.DatumBase.extend({
 
-    objectTypeClass: "datum datum-medium",
-
-    events: {
-        "focusin .element-input": function (ev) {
-            "use strict";
-            this.setActiveElement(ev.target);
-            ev.stopPropagation();
-        },
-        "focusout .element-input": function (ev) {
-            "use strict";
-            this.setActiveElement(ev.target);
-            ev.stopPropagation();
-        }
-    }
+    objectTypeClass: "datum datum-medium"
 
 });
