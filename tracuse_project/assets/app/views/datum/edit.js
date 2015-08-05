7f1450ac0bae61ebf3f37843966f0e08d27b7ca8
Sam Baron <sambaron@ownmail.net>
2015-08-05 08:48:26 -0700

    Render Elements in DatumEdit
    Redundant to DatumBase, but leave for now.

--- tracuse_project/assets/app/views/datum/edit.js
+++ tracuse_project/assets/app/views/datum/edit.js
@@ -5,5 +5,29 @@ Tracuse.views.DatumEdit = Tracuse.views.BaseEdit.extend({
 
     events: {},
 
+    render: function () {
+        "use strict";
+        /* Render Datum Elements using Element Type views
+         * */
+        var editView = Tracuse.views.BaseEdit.prototype.render.apply(this, arguments);
+        var elementContainer = editView.el.querySelector(".datum-elements");
+
+        var elementFrag = document.createDocumentFragment();
+        var elementCollection = editView.model.get("elements");
+        elementCollection.each(function (elementModel) {
+            var elementViewName = elementModel.get("element_type").get("element_view");
+            var ElementViewConst = Tracuse.views[elementViewName];
+            var elementView = new ElementViewConst({
+                model: elementModel,
+                parentView: editView
+            });
+            var elementEl = elementView.render().el;
+            elementFrag.appendChild(elementEl);
+        });
+
+        elementContainer.appendChild(elementFrag);
+
+        return editView;
+    }
 
 });
