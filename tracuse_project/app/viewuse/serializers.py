d57ba998e660fac48b899986f299a9c1b7a8c5f2
Sam Baron <sambaron@ownmail.net>
2015-08-05 15:52:43 -0700

    Add 'viewuse_arrangement' to ViewuseObjectSerializer.serial_update
    Add from UiObjectModelSerializer super class.

--- tracuse_project/app/viewuse/serializers.py
+++ tracuse_project/app/viewuse/serializers.py
@@ -8,9 +8,15 @@ class ViewuseObjectSerializer(UiObjectModelSerializer):
     model = ViewuseObject
 
     def serial_related(self):
-        output =  self.serial_default()
+        output = self.serial_default()
         output.append(("viewuse_arrangement", self.obj.viewuse_arrangement_id))
         return output
+
+    def serial_update(self):
+        output = super().serial_update()
+        output.append("viewuse_arrangement_id")
+        return output
+
 
 class ViewuseArrangementSerializer(Serializer):
     model = ViewuseArrangement
