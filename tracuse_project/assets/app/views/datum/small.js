fe8e91e1fb36c5b2e131cfe6ab7a9310667882d5
Sam Baron <sambaron@ownmail.net>
2015-08-11 16:52:19 -0700

    Do not render sub views in DatumSmall
    Causing a problem with recursive associations.

--- tracuse_project/assets/app/views/datum/small.js
+++ tracuse_project/assets/app/views/datum/small.js
@@ -1,6 +1,11 @@
 Tracuse.views.DatumSmall = Tracuse.views.DatumBase.extend({
 
     objectTypeClass: "datum datum-small",
-    templateName: "datum/small.html"
+    templateName: "datum/small.html",
+    
+    renderSubViews: function () {
+        "use strict";
+        return this;
+    }
 
 });
